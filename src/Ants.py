0b4f8cefcf912e9498a081ada4d91c63560df2d6
Antoine Zellmeyer <antoine.zellmeyer@epita.fr>
2022-12-09 18:45:56 +0100

    feat(Agent): Base agents behavior

--- src/Ants.py
+++ src/Ants.py
@@ -1,6 +1,11 @@
 import mesa
 import random
 
+ANT_LIFETIME = 50
+SOLDIER_ANT_ATTACK = 10
+
+REFILL_TRESHOLD = 30
+
 class Ants(mesa.Agent):
     def __init__(self, unique_id, model, position, nest_id):
         super().__init__(unique_id, model)
@@ -10,17 +15,27 @@ class Ants(mesa.Agent):
         self.color = "green"
         self.trace = 0
         self.alive = True
+        self.lifetime = ANT_LIFETIME
+        self.food_effect = 40
 
     def distance_to_target(self, target_x, target_y):
         return (self.posi[0] - target_x)**2 + (self.posi[1] - target_y)**2 
 
-
     def move_towards(self, goal_x, goal_y, min_dist, max_dist):
-        leap = random.uniform(min_dist, max_dist)
         direction_y = (goal_y - self.posi[1])
         direction_x = (goal_x - self.posi[0])
+        leap = random.uniform(min_dist, max_dist)
+        leap = min(abs(direction_x) + abs(direction_y), leap)
         self.posi[0] += direction_x * leap
         self.posi[1] += direction_y * leap
+
+    def refill_at_nest(self):
+        location = self.model.nest_list[self.nest_id].location
+        if self.distance_to_target(location[0], location[1]) > 2:
+            self.move_towards(location[0], location[0], 0, self.movespeed)
+        elif self.model.nest_list[self.nest_id].food_stock > 0:
+            self.model.nest_list[self.nest_id].food_stock -= 1
+            self.lifetime += self.food_effect
 
     def step(self):
         if not self.alive:
@@ -41,7 +56,6 @@ class Queen(Ants):
         self.antsSpawnRate = 10
         self.tick = 0
 
-
     def step(self):
         if not self.alive:
             return
@@ -49,6 +63,7 @@ class Queen(Ants):
         if self.hasReproduced:
             if self.tick >= self.antsSpawnRate:
                 baby = Ants(self.model.ids, self.model, list(self.posi), self.nest_id)
+                self.model.nest_list[self.nest_id].members.append(baby)
                 self.model.schedule.add(baby)
                 self.model.ids += 1
                 self.tick = 0
@@ -62,7 +77,6 @@ class Male(Ants):
     def step(self):
         if not self.alive:
             return
-
         queen_pos = self.model.nest_list[self.nest_id].location
         if self.distance_to_target(queen_pos[0], queen_pos[1]) > 2:
             self.move_towards(queen_pos[0], queen_pos[1], 0, self.movespeed)
@@ -70,4 +84,128 @@ class Male(Ants):
             self.model.nest_list[self.nest_id].queen.hasReproduced = True
             self.alive = False
             self.model.schedule.remove(self)
+
+class Explorer(Ants):
+    def __init__(self, unique_id, model, position, nest_id):
+        super().__init__(unique_id, model, position, nest_id)
+        self.color = "yellow"
+        self.carryFood = False
+        self.target = None
+        self.foodQte = 0
+
+    def go_back_home(self):
+        location = self.model.nest_list[self.nest_id].location
+        if self.distance_to_target(location[0], location[1]) > 2:
+            self.move_towards(location[0], location[0], 0, self.movespeed)
+        elif self.carryFood:
+            if self.foodQte > 1 and self.lifetime <= REFILL_TRESHOLD:
+                self.lifetime += self.food_effect
+                self.foodQte -= 1
+            self.model.nest_list[self.nest_id].food_stock += self.foodQte
+            self.foodQte = 0
+            self.carryFood = False
+            self.trace = 0
+        else:
+            self.trace = 0
+
+    def step(self):
+        if not self.alive:
+            return
+        self.lifetime -= 1
+        if self.lifetime <= 0:
+            self.alive = False
+            self.model.schedule.remove(self)
+        elif self.lifetime <= REFILL_TRESHOLD:
+            self.refill_at_nest()
+        else:
+            # TO OPTIMIZE
+            if not self.trace:
+                for food in self.model.foods:
+                    if food.pos == self.posi:
+                        self.trace = 1
+
+                if self.target is None or self.posi == self.target:
+                    self.target = [random.randint(0, self.model.width), random.randint(0, self.model.height)]
+
+                self.move_towards(self.target[0], self.target[1], 0, self.movespeed)
+            else:
+                self.go_back_home()
+                self.model.map[int(self.posi[1])][int(self.posi[0])].strength += self.trace
+
+
+# TO OPTIMIZE
+class Soldier(Ants):
+    def __init__(self, unique_id, model, position, nest_id):
+        super().__init__(unique_id, model, position, nest_id)
+        self.color = "green"
+        self.model.nest_list[self.nest_id].capacity += 10
+
+    def step(self):
+        if not self.alive:
+            return
+        self.chase_predator()
+        self.lifetime -= 1
+        if self.lifetime < 0:
+            self.alive = False
+            self.model.schedule.remove(self)
+
+    def chase_predator(self):
+        closest_predator = None
+        closest_dist = 1000
+        for predator in self.model.schedule.predators:
+            if predator.alive:
+                dist = self.distance_to_target(predator.posi[0], predator.posi[1])
+                if dist < closest_dist:
+                    closest_dist = dist
+                    closest_predator = predator
+        if closest_dist < 5:
+            self.move_towards(closest_predator.posi[0], closest_predator.posi[1], 0, self.movespeed)
+            closest_predator.lifetime -= SOLDIER_ANT_ATTACK
+            if closest_predator.lifetime < 0:
+                 closest_predator.alive = False
+        else:
+            self.chase_the_closest_explorer()
+
+    def chase_the_closest_explorer(self):
+        closest_explorer = None
+        closest_dist = 1000
+        for ant in self.model.nest_list[self.nest_id].members:
+            if isinstance(ant, Explorer):
+                if ant.alive:
+                    dist = self.distance_to_target(ant.posi[0], ant.posi[1])
+                    if dist < closest_dist:
+                        closest_dist = dist
+                        closest_explorer = ant
+        if closest_dist < 5:
+            self.move_towards(closest_explorer.posi[0], closest_explorer.posi[1], 0, self.movespeed)
+
+
+class Worker(Ants):
+    def __init__(self, unique_id, model, position, nest_id):
+        super().__init__(unique_id, model, position, nest_id)
+        self.color = "red"
+        self.target = None
+        self.wandering_range = 5
+        
+    def step(self):
+        if not self.alive:
+            return
+        self.lifetime -= 1
+        if self.lifetime <= 0:
+            self.alive = False
+            self.model.schedule.remove(self)
+        elif self.lifetime <= REFILL_TRESHOLD:
+            self.refill_at_nest()
+        else:
+            if self.model.map[int(self.posi[1])][int(self.posi[0])].strength > 0:
+                food_pos = self.model.find_food(self.posi[1],self.posi[0])
+                self.move_towards(food_pos[0], food_pos[1], 0, self.movespeed)
+            else:
+                if self.target is None or self.posi == self.target:
+                    nest_loc = self.model.nest_list[self.nest_id].location
+                    self.target = [nest_loc[0] + random.uniform(-self.wandering_range, self.wandering_range),
+                                   nest_loc[1] + random.uniform(-self.wandering_range, self.wandering_range)]
+
+                self.move_towards(self.target[0], self.target[1], 0, self.movespeed)
+    
 
--- src/environment.py
+++ src/environment.py
@@ -10,32 +10,64 @@ class Pheromone:
         self.type = 0
 
 
+class Food:
+    def __init__(self, serving, pos):
+        self.serving = serving
+        self.pos = pos
+
+
 class Nest:
     def __init__(self, queen, members):
         self.queen = queen
         self.members = members
         self.location = self.queen.posi
+        self.capacity = 10
+        self.food_stock = 0
+
 
 class AntsModel(mesa.Model):
     """
     Simulate the environment and its ants and nest inside it.
     """
+
     def __init__(self, nest_nb, width, height):
         self.nest_nb = nest_nb
         self.width = width
         self.height = height
         self.map = [[Pheromone() for x in range(self.width)] for y in range(self.height)]
+        self.foods_nb = 15
+        self.food_max_serving = 15
+        self.foods = []
+        self.__init_foods()
         self.nest_list = []
         self.schedule = mesa.time.RandomActivation(self)
         self.ids = 0
         for i in range(self.nest_nb):
             self.ids = self.__create_nest(self.ids)
 
+    def find_food(self, x, y):
+        for yi in range(-1, 2):
+            for xi in range(-1, 2):
+                if (not (yi == 0 and xi == 0)) and 0 <= x + xi < self.width and 0 <= y + yi < self.height:
+                    if self.map[y + yi][x + xi].strength != 0:
+                        return x + xi, y + yi
+
+    def __init_foods(self):
+        for food in self.foods_nb:
+            self.foods.append(Food(random.randint(1, self.food_max_serving),
+                                   [random.randint(0, self.width), random.randint(0, self.height)]))
+
+    def food_ticks(self):
+        food_total = len(self.foods)
+        while food_total < self.foods_nb:
+            self.foods.append(Food(random.randint(1, self.food_max_serving),
+                                   [random.randint(0, self.width), random.randint(0, self.height)]))
+
     def __create_nest(self, unique_id):
         nest_id = len(self.nest_list)
         rnd_pos = [random.randint(0, self.width), random.randint(0, self.height)]
         rnd_pos2 = [random.randint(0, self.width), random.randint(0, self.height)]
-        agent = Queen(unique_id , self, rnd_pos, nest_id)
+        agent = Queen(unique_id, self, rnd_pos, nest_id)
         agent_male = Male(unique_id + 1, self, rnd_pos2, nest_id)
         self.schedule.add(agent)
         self.schedule.add(agent_male)
@@ -43,14 +75,14 @@ class AntsModel(mesa.Model):
         return unique_id + 2
 
     def step(self):
+        self.food_ticks()
         self.schedule.step()
 
 
-
 if __name__ == "__main__":
-    model = AntsModel(2, 800,600)
+    model = AntsModel(2, 800, 600)
     for iteration in range(10):
         model.step()
         print("Current Ants location : ")
         for ants in model.schedule.agents:
-            print(" -> ", ants.posi) 
+            print(" -> ", ants.posi)
